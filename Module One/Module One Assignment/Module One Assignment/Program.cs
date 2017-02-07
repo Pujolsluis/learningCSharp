2b37f5615255bba376f303f84e0e6af87ff5ec77
Luis Pujols <luispujols@hotmail.com>
2017-02-07 12:06:14 -0400

    ADD 'Created the Module One Assignment basic solution file and created the variables for the student'

---
+++ Module One/Module One Assignment/Module One Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_One_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, LastName;
+            DateTime birthday;
+            string addressLine1, addressLine2;
+            string city, stateProvince, zipPostalCode, country;
+        }
+    }
+}
