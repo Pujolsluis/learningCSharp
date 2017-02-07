036a8ba7c24f0f3ec5dddfd3895c0b9eeb42fc5b
Luis Pujols <luispujols@hotmail.com>
2017-02-06 23:19:08 -0400

    ADD 'Created the first demo of the course, about Data Types, Variables and Operators'

---
+++ Module One/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 2;
+            int secondVar;
+            secondVar = 10;
+
+            int sum = myVar + secondVar;
+            int product = myVar + secondVar;
+            int remainder = myVar % secondVar;
+            double value = (double)myVar / (double)secondVar;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(product);
+            Console.WriteLine(remainder);
+            Console.WriteLine(value);
+        }
+    }
+}
