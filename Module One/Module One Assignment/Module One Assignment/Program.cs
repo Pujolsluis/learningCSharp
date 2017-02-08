deed158559980be85653b27e03901c44e396f336
Luis Pujols <luispujols@hotmail.com>
2017-02-07 20:53:04 -0400

    ADD 'Created the user input logic for the assignment, now the program succesfully gathers information from the user'

--- Module One/Module One Assignment/Module One Assignment/Program.cs
+++ Module One/Module One Assignment/Module One Assignment/Program.cs
@@ -10,10 +10,34 @@ namespace Module_One_Assignment
     {
         static void Main(string[] args)
         {
-            string firstName, LastName;
-            DateTime birthday;
+            string firstName, lastName;
+            int id, zipPostalCode;
+            string birthday;
             string addressLine1, addressLine2;
-            string city, stateProvince, zipPostalCode, country;
+            string city, stateProvince, country;
+
+            Console.WriteLine("Student First Name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Student Last Name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Student ID");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Student birthday:");
+            birthday = Console.ReadLine();
+            Console.WriteLine("Student Address 1:");
+            addressLine1 = Console.ReadLine();
+            Console.WriteLine("Student Address 2:");
+            addressLine2 = Console.ReadLine();
+            Console.WriteLine("Student city:");
+            city = Console.ReadLine();
+            Console.WriteLine("Student State/Province:");
+            stateProvince = Console.ReadLine();
+            Console.WriteLine("Student ZIP/Postal Code:");
+            zipPostalCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Student Country:");
+            country = Console.ReadLine();
+
+           
         }
     }
 }
