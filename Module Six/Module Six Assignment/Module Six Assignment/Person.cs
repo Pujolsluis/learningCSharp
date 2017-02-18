560c82bac185a325c52f7f5de500666b796705a6
Luis Pujols <luispujols@hotmail.com>
2017-02-18 15:20:19 -0400

    ADD 'Created the Person Class for the assignment'

---
+++ Module Six/Module Six Assignment/Module Six Assignment/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Six_Assignment
+{
+    class Person
+    {
+        string mFirstName { get; set; }
+        string mLastName { get; set; }
+        string mBirthday { get; set; }
+        string mAddress1 { get; set; }
+        string mAddress2 { get; set; }
+        int mPostalCode { get; set; }
+        string mCountry { get; set; }
+        string mState { get; set; }
+
+        public Person(string name, string lastname, string birthday, string address1, string address2
+            , int postalcode, string country, string state)
+        {
+            mFirstName = name;
+            mLastName = lastname;
+            mBirthday = birthday;
+            mAddress1 = address1;
+            mAddress2 = address2;
+            mPostalCode = postalcode;
+            mCountry = country;
+            mState = state;
+        }
+    }
+}
