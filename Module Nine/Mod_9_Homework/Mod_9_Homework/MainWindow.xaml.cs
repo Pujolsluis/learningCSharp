ebc18f3faa89fbf6ce03a3b7de7c7bdd33b7e2b9
Luis Pujols <luispujols@hotmail.com>
2017-03-01 19:34:15 -0400

    ADD 'Created the logic for the module nine assignment and now my assignment is completed'

--- Module Nine/Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
+++ Module Nine/Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace Mod_9_Homework
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> students { get; set; }
+        int index = -1;
         public MainWindow()
         {
             InitializeComponent();
+            students = new List<Student>();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName, lastName, city;
+            firstName = txtFirstName.Text;
+            lastName = txtLastName.Text;
+            city = txtCity.Text;
+            Student newStudent = new Student(firstName, lastName, city);
+            students.Add(newStudent);
+            string message = "Student " + firstName + " " + lastName + " Has been created, amount of registered students " + students.Count;
+            MessageBox.Show(message);
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            index++;
+            if (students.Count == 0)
+            {
+                MessageBox.Show("There are no students registered.");
+                return;
+            }
+            if(index >= students.Count)
+            {
+                index = 0;
+            }
+            txtFirstName.Text = students[index].mFirstName;
+            txtLastName.Text = students[index].mLastName;
+            txtCity.Text = students[index].mCity;
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            index--;
+            if (students.Count == 0)
+            {
+                MessageBox.Show("There are no students registered.");
+                return;
+            }
+            if (index < 0)
+            {
+                index = (students.Count-1);
+            }
+            txtFirstName.Text = students[index].mFirstName;
+            txtLastName.Text = students[index].mLastName;
+            txtCity.Text = students[index].mCity;
         }
     }
 }
---
+++ Module Nine/Mod_9_Homework/Mod_9_Homework/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student
+    {
+        public string mFirstName { get; set; }
+        public string mLastName { get; set; }
+        public string mCity { get; set; }
+
+        public Student(string firstName, string lastName, string city)
+        {
+            mFirstName = firstName;
+            mLastName = lastName;
+            mCity = city;
+        }
+    }
+}
