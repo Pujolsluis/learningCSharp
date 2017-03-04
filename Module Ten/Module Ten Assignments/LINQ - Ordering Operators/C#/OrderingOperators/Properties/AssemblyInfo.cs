d48a837fa1a8fae3118e5f980dfc32e1b489b38a
Luis Pujols <luispujols@hotmail.com>
2017-03-03 22:55:38 -0400

    ADD 'Created the two assignments of module ten basic files'

--- Module Ten/Module Ten Assignments/LINQ - Ordering Operators/C#/OrderingOperators/Properties/AssemblyInfo.cs
+++ Module Ten/Module Ten Assignments/LINQ - Ordering Operators/C#/OrderingOperators/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Module Ten Assignment")]
+[assembly: AssemblyTitle("OrderingOperators")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Module Ten Assignment")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("OrderingOperators")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cfb7bc44-2516-43b2-9974-5eee3710e0b2")]
+[assembly: Guid("a3d03575-f298-4fd8-8a13-f4ce9f08869a")]
 
 // Version information for an assembly consists of the following four values:
 //
